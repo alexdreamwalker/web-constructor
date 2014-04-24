be917c1dd6108d2572d7a874950fb622ba0bbd68
dreamer <dreamer@dreamer-ThinkPad-X121e.(none)>
2014-04-24 23:04:10 +0600

    Designer JS class

---
+++ js/sunblind/designer.js
@@ -0,0 +1,47 @@
+function Designer(options) {
+	this.elem = options.elem;
+	this.filterInput = null;
+	this.filterButton = null;
+	this.table = null;
+	this.acceptButton = null;
+	var self = this;
+
+	this.init = function() {
+		this.filterInput = document.querySelector("#" + this.elem + " input");
+		this.filterInput.addEventListener("input", this.filterColors, false);
+		this.filterButton = document.querySelectorAll("#" + this.elem + " button")[1];
+		this.filterButton.addEventListener("click", this.filterColors, false);
+		this.table = document.querySelector("#" + this.elem + " tbody");
+		this.acceptButton = document.querySelectorAll("#" + this.elem + " button")[3];
+		this.acceptButton.addEventListener("click", this.acceptColor, false);
+
+		var rows = this.table.querySelectorAll("tr");
+		for(var i = 0; i < rows.length; i++) rows[i].addEventListener("click", this.setColor, false);
+	};
+
+	this.filterColors = function() {
+		var text = self.filterInput.value;
+		var rows = self.table.querySelectorAll("tr");
+		for(var i = 0; i < rows.length; i++) {
+			var name = rows[i].querySelectorAll("td")[2].innerHTML;
+			if(name.indexOf(text) == -1)
+				rows[i].style.display = "none";
+			else rows[i].style.display = "";
+		}
+	};
+
+	this.setColor = function(e) {
+		var target = e.target.parentNode;
+		alert(target);
+		var material = {};
+		material.id = target.dataset.id;
+		material.url = target.querySelector("img").src;
+		global.ui.applyColor(material);
+	};
+
+	this.acceptColor = function() {
+		global.ui.hideDesigner();
+	};
+
+	this.init();
+};
